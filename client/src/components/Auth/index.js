dcfde55c31a551a53be617fd7dee0baee198b5a6
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2021-01-21 17:17:35 +0100

    implementation de l'authentification avec un compte Google

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-
+import { GOOGLE_CLIENT_ID } from '../../googleAuth';
+import Icon from './icon';
 import Input from './input';
 
 //Styles
@@ -10,8 +12,8 @@ import useStyles from './style';
 
 const Auth = () => {
   const classes = useStyles();
+  const [isSignup, setIsSignup] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const isSignup = false;
 
   const handleShowPassword = () => setShowPassword(!showPassword);
 
@@ -22,6 +24,32 @@ const Auth = () => {
   const handleChange = () => {
 
   };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+  // const googleSuccess = async (res) => {
+  //   const result = res?.profileObj;
+  //   const token = res?.tokenId;
+
+  //   try {
+  //     dispatch({ type: AUTH, data: { result, token } });
+
+  //     history.push('/');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  const googleSuccess = (res) => {
+    console.log(res);
+  };
+
+  const googleError = () => alert('Oups... Connexion avec compte Google refusé. Essayez de nouveau.');
+
+  //const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
 
   return (
     <Container component="main" maxWidth="xs">
@@ -47,6 +75,32 @@ const Auth = () => {
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             { isSignup ? 'Inscription' : 'Connexion' }
           </Button>
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            render={(renderProps) => (
+              <Button 
+                className={classes.googleButton} 
+                color="primary" 
+                fullWidth 
+                onClick={renderProps.onClick} 
+                //disabled={renderProps.disabled} 
+                startIcon={<Icon />} 
+                variant="contained"
+              >
+                Se connecter avec Google
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleError}
+            cookiePolicy="single_host_origin"
+          />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                { isSignup ? 'J\'ai un compte ? Connexion' : "Je n'ai pas de compte ? Inscription" }
+              </Button>
+            </Grid>
+          </Grid>
         </form>
       </Paper>
     </Container>
