9d59ad5a08cdd1a1accfbc9e55cbd3f88cb38606
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2021-01-26 12:03:29 +0100

    limitation edition, suppression et like des posts pour utilisateurs autorisés

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -11,12 +11,13 @@ import useStyles from './style';
 
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({author: '', title: '', content: '', tags: '', urlImg: ''});
+  const [postData, setPostData] = useState({title: '', content: '', tags: '', urlImg: ''});
   const [savePost, setSavePost] = useState(false);
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
   const [image, setImage] = useState(null);
+  const user = JSON.parse(localStorage.getItem('profile'));
 
   useEffect(() => {
     if (post) setPostData(post);
@@ -24,7 +25,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
   useEffect(() => {
     if (savePost && currentId === null) {
-      dispatch(createPost(postData));
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
       console.log("nouveau projet enregistré sur mongoDB");
       clear();
     }
@@ -40,7 +41,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
   const clear = () => {
     setCurrentId(null);
-    setPostData({ author: '', title: '', content: '', tags: '', urlImg: '' });
+    setPostData({ title: '', content: '', tags: '', urlImg: '' });
   };
 
   const handleSubmit = async (e) => {
@@ -66,11 +67,20 @@ const Form = ({ currentId, setCurrentId }) => {
     });
   };
 
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Pour creer ou liker les autres posts, veuillez-vous inscrire.
+        </Typography>
+      </Paper>
+    )
+  }
+
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{currentId ? 'Editer' : 'Créer' } un projet web</Typography>
-        <TextField name="author" variant="outlined" label="Author" fullWidth value={postData.author} onChange={(e) => setPostData({ ...postData, author: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="content" variant="outlined" label="Content" fullWidth value={postData.content} onChange={(e) => setPostData({ ...postData, content: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
