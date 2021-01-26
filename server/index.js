a964a9b23ad5cc6e299213137c6f2319c9a8f0b3
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2021-01-26 10:26:45 +0100

    mise en place de la gestion de l'inscription avec middelware auth et jwt

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
 dotenv.config();
 const app = express();
 
@@ -13,10 +15,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello to Portfolio API');
-});
+app.use('/user', userRoutes);
 
 const databaseURL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
