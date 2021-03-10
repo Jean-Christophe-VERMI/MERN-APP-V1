ef3148048abcd7ee7c8215c7533a07aa5eefd461
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2021-03-10 18:40:33 +0100

    cors origin heroku app client - server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import contactRoute from './routes/contact.js';
 const app = express();
 const { MONGO_URI, MONGO_DB_NAME } = config;
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://deploymern2021.herokuapp.com'}));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use('/', contactRoute);
